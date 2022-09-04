930caa48c2b3e4622e9a709b0351689001816a76
benmandakh2609 <benmandakh@gmail.com>
2022-09-04 01:43:09 -0500

    first commit

---
+++ src/components/Data.jsx
@@ -0,0 +1,14 @@
+export const countries = [
+    {
+      image: "https://picsum.photos/200/300",
+      title: "Brazil",
+    },
+    {
+      image: "https://picsum.photos/200/400",
+      title: "China",
+    },
+    {
+      image: "https://picsum.photos/200/500",
+      title: "France",
+    },
+  ];
